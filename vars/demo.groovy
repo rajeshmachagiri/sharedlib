5e27e795e292993c3f5ec540842a6c58cc45361a
Your Name <you@example.com>
2023-12-26 16:55:14 +0530

    changes

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -37,39 +37,39 @@ def call() {
                     }
                 }
             }
-            stage('Build-Jar-file') {
-                parallel {
-                    stage('docker build') {
-                        steps {
-                            container('docker') {
-
-                                dir('users-api'){
-
-                                    sh 'docker ps'
-                                    sh 'docker login -u=rajeshmachagiri -p=8686548640rrR!'
-                                    sh 'docker build -t users-api .'
-                                    sh 'docker tag users-api rajeshmachagiri/users-api:latest'
-                                    sh 'docker push rajeshmachagiri/users-api:latest'
-                                }
-                            }
-                        }
-                    }
-                    stage('docker-build2') {
-                        steps {
-                            container('docker') {
-                                dir('todos-api'){
-                                    sh 'docker ps'
-                                    sh 'docker login -u=rajeshmachagiri -p=8686548640rrR!'
-                                    sh 'docker build -t auth-api .'
-                                    sh 'docker tag auth-api rajeshmachagiri/auth-api:latest'
-                                    sh 'docker push rajeshmachagiri/auth-api:latest'
-                                }
-                            }
-                        }
-                    }
-
-                }
-            }
+//            stage('Build-Jar-file') {
+//                parallel {
+//                    stage('docker build') {
+//                        steps {
+//                            container('docker') {
+//
+//                                dir('users-api'){
+//
+//                                    sh 'docker ps'
+//                                    sh 'docker login -u=rajeshmachagiri -p=8686548640rrR!'
+//                                    sh 'docker build -t users-api .'
+//                                    sh 'docker tag users-api rajeshmachagiri/users-api:latest'
+//                                    sh 'docker push rajeshmachagiri/users-api:latest'
+//                                }
+//                            }
+//                        }
+//                    }
+//                    stage('docker-build2') {
+//                        steps {
+//                            container('docker') {
+//                                dir('todos-api'){
+//                                    sh 'docker ps'
+//                                    sh 'docker login -u=rajeshmachagiri -p=8686548640rrR!'
+//                                    sh 'docker build -t auth-api .'
+//                                    sh 'docker tag auth-api rajeshmachagiri/auth-api:latest'
+//                                    sh 'docker push rajeshmachagiri/auth-api:latest'
+//                                }
+//                            }
+//                        }
+//                    }
+//
+//                }
+//            }
             stage('kubectl'){
                 steps {
                     sh "curl -LO https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
