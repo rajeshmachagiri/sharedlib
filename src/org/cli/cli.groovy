345b6337653b636d3a222373b29af7ec68a4adb4
Your Name <you@example.com>
2023-12-26 15:08:18 +0530

    changes

--- src/org/cli/cli.groovy
+++ src/org/cli/cli.groovy
@@ -3,11 +3,11 @@ package org.cli
 interface cli{
     def name(String args)
 
-    warn(String arg)
+    def warn(String arg)
 
-    error(String arg)
+    def error(String arg)
 
-    sh(String arg)
+    def sh(String arg)
 
 }
 
