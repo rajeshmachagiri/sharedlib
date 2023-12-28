8f77a4ab9f72a42c744ee208674d5925bc90af4a
Your Name <you@example.com>
2023-12-28 10:02:21 +0530

    changes

---
+++ src/org/cli/docker/Docker.groovy
@@ -0,0 +1,21 @@
+package org.cli.docker
+
+import org.cli.Steps
+
+class Docker implements dockerinterface{
+Steps link = new Steps()
+    @Override
+    def login() {
+        link.withenv(['DOCKER_REGISTRY_USER=rajeshmachagiri','DOCKER_REGISTRY_PASS=dckr_pat_EhSNYMctetlqFc-p2uuY_VrSQHI'],"login",'echo "$DOCKER_REGISTRY_PASS" | docker login $DOCKER_REGISTRY --username $DOCKER_REGISTRY_USER --password-stdin')
+    }
+
+    @Override
+    def auth() {
+        return null
+    }
+
+    @Override
+    def push() {
+        return null
+    }
+}
