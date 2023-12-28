cdf2ab01eb8702577b4aedd5c08282047bd35bb6
Your Name <you@example.com>
2023-12-28 10:53:15 +0530

    changes

--- vars/dockercall.groovy
+++ vars/dockercall.groovy
@@ -1,57 +1,58 @@
 #!/usr/bin/env groovy
 import org.cli.docker.DockerDevil
 
-
-pipeline {
-    agent {
-        kubernetes {
-            yaml '''
-        apiVersion: v1
-        kind: Pod
-        spec:
-          containers:
-          - name: maven
-            image: maven:alpine
-            command:
-            - cat
-            tty: true
-          - name: docker
-            image: docker:19.03.1-dind
-            securityContext:
-              privileged: true
-            env:
-              - name: DOCKER_TLS_CERTDIR
-                value: ""
-        '''
-        }
-    }
-    stages {
-        stage('init'){
-            steps{
-                script{
-                    demo("let it go")
-
-                }
+call() {
+    pipeline {
+        agent {
+            kubernetes {
+                yaml '''
+            apiVersion: v1
+            kind: Pod
+            spec:
+              containers:
+              - name: maven
+                image: maven:alpine
+                command:
+                - cat
+                tty: true
+              - name: docker
+                image: docker:19.03.1-dind
+                securityContext:
+                  privileged: true
+                env:
+                  - name: DOCKER_TLS_CERTDIR
+                    value: ""
+            '''
             }
         }
-        stage('Build-Jar-file') {
-            parallel {
-                stage('docker build') {
-                    steps {
-                        container('docker') {
+        stages {
+            stage('init') {
+                steps {
+                    script {
+                        demo("let it go")
 
-                            script{
+                    }
+                }
+            }
+            stage('Build-Jar-file') {
+                parallel {
+                    stage('docker build') {
+                        steps {
+                            container('docker') {
 
-                                DockerDevil dante = new DockerDevil()
-                                dante.login()
+                                script {
+
+                                    DockerDevil dante = new DockerDevil()
+                                    dante.login()
 
 
+                                }
                             }
                         }
                     }
                 }
             }
         }
-    }
 
+    }
 }
