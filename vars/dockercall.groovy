ea20f90158be52920bd25d009da5d14561423b70
Your Name <you@example.com>
2023-12-28 10:51:48 +0530

    changes

--- vars/dockercall.groovy
+++ vars/dockercall.groovy
@@ -1,7 +1,57 @@
+#!/usr/bin/env groovy
 import org.cli.docker.DockerDevil
 
-call(){
-    DockerDevil doc = new DockerDevil()
-    doc.login()
-}
 
+pipeline {
+    agent {
+        kubernetes {
+            yaml '''
+        apiVersion: v1
+        kind: Pod
+        spec:
+          containers:
+          - name: maven
+            image: maven:alpine
+            command:
+            - cat
+            tty: true
+          - name: docker
+            image: docker:19.03.1-dind
+            securityContext:
+              privileged: true
+            env:
+              - name: DOCKER_TLS_CERTDIR
+                value: ""
+        '''
+        }
+    }
+    stages {
+        stage('init'){
+            steps{
+                script{
+                    demo("let it go")
+
+                }
+            }
+        }
+        stage('Build-Jar-file') {
+            parallel {
+                stage('docker build') {
+                    steps {
+                        container('docker') {
+
+                            script{
+
+                                DockerDevil dante = new DockerDevil()
+                                dante.login()
+
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
