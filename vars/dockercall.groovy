e08223b475387ac4e2e5eda72fe89b70d95abba1
Your Name <you@example.com>
2023-12-28 11:07:21 +0530

    changes

--- vars/dockercall.groovy
+++ vars/dockercall.groovy
@@ -47,9 +47,40 @@ def call() {
 
 
                                 }
+                                dir('users-api'){
+
+                                    sh 'docker ps'
+                                    sh 'docker build -t users-api .'
+                                    sh 'docker tag users-api rajeshmachagiri/users-api:latest'
+                                    sh 'docker push rajeshmachagiri/users-api:latest'
+                                }
+                            }
+
+                        }
+                    }
+                    stage('docker-build2') {
+                        steps {
+                            container('docker') {
+                                dir('todos-api'){
+                                    sh 'docker ps'
+                                    sh 'docker login -u=rajeshmachagiri -p=8686548640rrR!'
+                                    sh 'docker build -t auth-api .'
+                                    sh 'docker tag auth-api rajeshmachagiri/auth-api:latest'
+                                    sh 'docker push rajeshmachagiri/auth-api:latest'
+                                }
                             }
                         }
                     }
+
+                }
+            }
+            stage('kubectl'){
+                steps {
+                    sh "curl -LO https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
+                    sh "chmod +x kubectl"
+                    sh "./kubectl get pods"
+                    sh "./kubectl apply -f ./k8s/users-api"
+                    sh "./kubectl apply -f ./k8s/todos-api"
                 }
             }
         }
