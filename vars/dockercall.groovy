7c035c3f4a7185f766a62a731bb928b5a60a62d0
Your Name <you@example.com>
2023-12-29 14:18:54 +0530

    changes

--- vars/dockercall.groovy
+++ vars/dockercall.groovy
@@ -58,6 +58,8 @@ def call() {
 
                                     script {
                                         sh '''apt update -y
+apt install curl -y
+apt install unzip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install'''
