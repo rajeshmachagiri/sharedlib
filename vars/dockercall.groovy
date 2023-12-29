a6097b98052319b459ce5db87dd4512a80b48e9a
Your Name <you@example.com>
2023-12-29 14:16:47 +0530

    changes

--- vars/dockercall.groovy
+++ vars/dockercall.groovy
@@ -57,10 +57,10 @@ def call() {
                                 dir('users-api'){
 
                                     script {
-                                        sh '''sudo apt update -y
+                                        sh '''apt update -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ./aws/install'''
+./aws/install'''
                                         Docker = new DockerDevil()
                                         Docker.login()
 
